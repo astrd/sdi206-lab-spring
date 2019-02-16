3a3bd131563cd47d2c0a5233b8071480b8751b33
astrid <UO258194@uniovi.es>
2019-02-16 17:15:39 +0100

    SDI-SpringBoot- 6 - Validación de datos en el servidor

--- sdi206-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi206-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -5,6 +5,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.uniovi.entities.User;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.SignUpFormValidator;
 
 @Controller
 public class UsersController {
@@ -21,6 +24,8 @@ public class UsersController {
 
 	@Autowired
 	private SecurityService securityService;
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
 
 	@RequestMapping("/user/list")
 	public String getListado(Model model) {
@@ -67,12 +72,18 @@ public class UsersController {
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public String signup() {
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
 		return "signup";
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(@ModelAttribute("user") User user, Model model) {
+	public String signup(@Validated User user, BindingResult result, Model model) {
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors()) {
+		return "signup";
+		}
+
 		usersService.addUser(user);
 		securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
 		return "redirect:home";
@@ -83,13 +94,14 @@ public class UsersController {
 		return "login";
 	}
 
-	
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
 	public String home(Model model) {
-	 Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	 String dni = auth.getName();
-	 User activeUser = usersService.getUserByDni(dni);
-	 model.addAttribute("markList", activeUser.getMarks());
-	 return "home";
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String dni = auth.getName();
+		User activeUser = usersService.getUserByDni(dni);
+		model.addAttribute("markList", activeUser.getMarks());
+		return "home";
 	}
+
+	
 }
