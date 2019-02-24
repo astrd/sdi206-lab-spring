6d86ebd8814f9bd4f9a18df0456b88435b5d2358
astrid <UO258194@uniovi.es>
2019-02-24 12:21:26 +0100

    SDI-SpringBoot-Sesión-4.1-Sesiones

---
+++ sdi206-lab-spring/src/main/java/com/uniovi/services/MarksService.java
@@ -0,0 +1,49 @@
+package com.uniovi.services;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uniovi.entities.Mark;
+import com.uniovi.repositories.MarksRepository;
+
+@Service
+public class MarksService {
+	@Autowired
+	private HttpSession httpSession;
+
+	@Autowired
+	private MarksRepository marksRepository;
+
+	public List<Mark> getMarks() {
+		List<Mark> marks = new ArrayList<Mark>();
+		marksRepository.findAll().forEach(marks::add);
+		return marks;
+	}
+
+	public Mark getMark(Long id) {
+		Set<Mark> consultedList = (Set<Mark>) httpSession.getAttribute("consultedList");
+		if (consultedList == null) {
+			consultedList = new HashSet<Mark>();
+		}
+		Mark markObtained = marksRepository.findById(id).get();
+		consultedList.add(markObtained);
+		httpSession.setAttribute("consultedList", consultedList);
+		return markObtained;
+	}
+
+	public void addMark(Mark mark) {
+// Si en Id es null le asignamos el ultimo + 1 de la lista
+		marksRepository.save(mark);
+	}
+
+	public void deleteMark(Long id) {
+		marksRepository.deleteById(id);
+	}
+}
