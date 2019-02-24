9f438804a4cece3b0f324b138d14e17336b4ebe5
astrid <UO258194@uniovi.es>
2019-02-24 14:16:53 +0100

    “SDI-SpringBoot-Sesión-4.4-Consulta”

--- sdi206-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi206-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,4 +1,7 @@
 package com.uniovi.controllers;
+
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,44 +9,51 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.services.MarksService;
 import com.uniovi.services.UsersService;
+
 @Controller
 public class MarksController {
-	@Autowired //Inyectar el servicio
+	@Autowired // Inyectar el servicio
 	private MarksService marksService;
-	
+
 	@Autowired
 	private UsersService usersService;
-	
-	@RequestMapping("/mark/list")	
-	public String getList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
-		return "mark/list"; 
+
+	@RequestMapping(value = "/mark/{id}/resend", method = RequestMethod.GET)
+	public String setResendTrue(Model model, @PathVariable Long id) {
+		marksService.setMarkResend(true, id);
+		return "redirect:/mark/list";
 	}
-	@RequestMapping(value="/mark/{id}/resend", method=RequestMethod.GET)
-	public String setResendTrue(Model model, @PathVariable Long id){
-	marksService.setMarkResend(true, id);
-	return "redirect:/mark/list";
+
+	@RequestMapping(value = "/mark/{id}/noresend", method = RequestMethod.GET)
+	public String setResendFalse(Model model, @PathVariable Long id) {
+		marksService.setMarkResend(false, id);
+		return "redirect:/mark/list";
 	}
-	@RequestMapping(value="/mark/{id}/noresend", method=RequestMethod.GET)
-	public String setResendFalse(Model model, @PathVariable Long id){
-	marksService.setMarkResend(false, id);
-	return "redirect:/mark/list";
+
+	@RequestMapping("/mark/list")
+	public String getList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
+		return "mark/list";
 	}
-  
+
 	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
 	public String setMark(@ModelAttribute Mark mark) {
 
 		marksService.addMark(mark);
 		return "redirect:/mark/list";
 	}
-	
-	@RequestMapping(value="/mark/add")
-	public String getMark(Model model){
-	model.addAttribute("usersList", usersService.getUsers());
-	return "mark/add";
+
+	@RequestMapping(value = "/mark/add")
+	public String getMark(Model model) {
+		model.addAttribute("usersList", usersService.getUsers());
+		return "mark/add";
 	}
 
 	@RequestMapping("/mark/details/{id}")
@@ -58,7 +68,6 @@ public class MarksController {
 		return "redirect:/mark/list";
 	}
 
-	
 	@RequestMapping(value = "/mark/edit/{id}")
 	public String getEdit(Model model, @PathVariable Long id) {
 		model.addAttribute("mark", marksService.getMark(id));
@@ -66,23 +75,22 @@ public class MarksController {
 		return "mark/edit";
 	}
 
-	@RequestMapping(value="/mark/edit/{id}", method=RequestMethod.POST)
-	public String setEdit(Model model, @PathVariable Long id, @ModelAttribute Mark mark){
-	Mark original = marksService.getMark(id);
-	// modificar solo score y description
-	original.setScore(mark.getScore());
-	original.setDescription(mark.getDescription());
-	marksService.addMark(original);
-	return "redirect:/mark/details/"+id;
+	@RequestMapping(value = "/mark/edit/{id}", method = RequestMethod.POST)
+	public String setEdit(Model model, @PathVariable Long id, @ModelAttribute Mark mark) {
+		Mark original = marksService.getMark(id);
+		// modificar solo score y description
+		original.setScore(mark.getScore());
+		original.setDescription(mark.getDescription());
+		marksService.addMark(original);
+		return "redirect:/mark/details/" + id;
 	}
 
 	@RequestMapping("/mark/list/update")
-	public String updateList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String updateList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list :: tableMarks";
 	}
-	
-	
-
 
 }
--- sdi206-lab-spring/src/main/java/com/uniovi/services/MarksService.java
+++ sdi206-lab-spring/src/main/java/com/uniovi/services/MarksService.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.repositories.MarksRepository;
 
 @Service
@@ -38,7 +39,16 @@ public class MarksService {
 		}
 	}
 
-	
+	public List<Mark> getMarksForUser (User user){
+		List<Mark> marks = new ArrayList<Mark>();
+		if ( user.getRole().equals("ROLE_STUDENT")) {
+		marks = marksRepository.findAllByUser(user);
+		}
+		if ( user.getRole().equals("ROLE_PROFESSOR")){
+		marks = getMarks();
+		}
+		return marks;
+		}
 
 	public Mark getMark(Long id) {
 		Set<Mark> consultedList = (Set<Mark>) httpSession.getAttribute("consultedList");
